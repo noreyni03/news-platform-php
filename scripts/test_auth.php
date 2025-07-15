b657a5764966d1121e226025a324e57e4915a05f
noreyni03 <onoreyni03@gmail.com>
2025-07-15 16:22:24 +0100

    feat: Implémentation du GUI du desktop client

--- scripts/test_auth.php
+++
@@ -1,4 +0,0 @@
-<?php
-$client = new SoapClient('http://localhost/projet-actualite/backend/api/soap_server.php?wsdl');
-$response = $client->authenticateUser('admin','password');
-var_dump($response);
